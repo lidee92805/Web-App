e9ba8493a342b2d76cd4258eee0d92891a9cf1bb
LiDehua <lidehua@XXXdeMacbook-Pro.local>
2016-10-17 11:26:49 +0800

    Init

---
+++ Sources/App/main.swift
@@ -0,0 +1,24 @@
+import Vapor
+
+let drop = Droplet()
+
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+drop.get("/name", ":name") { request in
+    if let name = request.parameters["name"]?.string {
+        return "Hello \(name)!"
+    }
+    return "Error retrieving parameters."
+}
+
+drop.get("/view") { request in
+    return try drop.view.make("view.html")
+}
+
+drop.resource("posts", PostController())
+
+drop.run()
